6fa1b9a5e5acfefca603c12d3c8cf0557d79acf8
Dhaval <71810466+Dhavalgj2@users.noreply.github.com>
2023-04-15 17:41:35 +1000

    final changes

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
 import { useEffect } from "react";
-import { showNotification } from "./Store/cartSlice";
 import Notification from "../src/components/Cart/Notification";
+import { sendCartData } from "./Store/cartSlice";
+
+let isInitial = true;
 
 function App() {
   const showCart = useSelector((state) => state.cart.isShown);
@@ -13,44 +15,11 @@ function App() {
   const notification = useSelector((state) => state.cart.notification);
 
   useEffect(() => {
-    const sendCartData = async () => {
-      dispatch(
-        showNotification({
-          title: "Sending...",
-          message: "Sending Cart Data",
-          status: "Pending",
-        })
-      );
-
-      const response = await fetch(
-        "https://foodorderhttps-default-rtdb.firebaseio.com/cart.json",
-        {
-          method: "PUT",
-          body: JSON.stringify(cart),
-        }
-      );
-      if (!response.ok) {
-        // throw new Error("Something wrong while sending the Data!");
-      }
-      const data = await response.json();
-
-      dispatch(
-        showNotification({
-          title: "success",
-          message: "Sending Cart Data Successfully",
-          status: "success!",
-        })
-      );
-    };
-    sendCartData().catch((error) => {
-      dispatch(
-        showNotification({
-          title: "Error!",
-          message: "Sending Cart Data failed",
-          status: "error",
-        })
-      );
-    });
+    if (isInitial) {
+      isInitial = false;
+      return;
+    }
+    dispatch(sendCartData(cart));
   }, [cart, dispatch]);
   return (
     <>
--- src/Store/cartSlice.js
+++ src/Store/cartSlice.js
@@ -17,6 +17,49 @@ const cartSlice = createSlice({
   },
 });
 
+export const sendCartData = (cart) => {
+  return async (dispatch) => {
+    dispatch(
+      showNotification({
+        status: "pending",
+        title: "Sending...",
+        message: "Sending Cart Data",
+      })
+    );
+
+    const sendRequest = async () => {
+      const response = await fetch(
+        "https://foodorderhttps-default-rtdb.firebaseio.com/cart.json",
+        {
+          method: "PUT",
+          body: JSON.stringify(cart),
+        }
+      );
+      if (!response.ok) {
+        throw new Error("Something wrong while sending the Data!");
+      }
+    };
+
+    try {
+      await sendRequest();
+      dispatch(
+        showNotification({
+          title: "success",
+          message: "sent cart data",
+          status: "success!",
+        })
+      );
+    } catch (error) {
+      dispatch(
+        showNotification({
+          title: "error!",
+          message: "Sending Cart Data failed",
+          status: "error",
+        })
+      );
+    }
+  };
+};
 export const cartReducer = cartSlice.reducer;
 
 export const { toggle, showNotification } = cartSlice.actions;
